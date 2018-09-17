3f8247436f5c3e21eb8093ab58c320ff2a37c012
Bruno Nakahara Hirata <bruno.n.hirata@outlook.com>
2018-09-17 01:28:04 -0300

    Criação da rota Almanaque

---
+++ PortalClubeConteza/Controllers/AlmanaqueController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace PortalClubeConteza.Controllers
+{
+    public class AlmanaqueController : Controller
+    {
+        public ActionResult Index()
+        {
+            var arquivoPdf = System.IO.File.ReadAllBytes(Server.MapPath("~/Content/File/Almanaque/Almanaque.pdf"));
+            return File(arquivoPdf, "application/pdf");
+        }
+    }
+}
